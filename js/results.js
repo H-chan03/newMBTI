5f78fa00b4f4865cb4715cc6de0773a3fc3088b6
H-chan03 <158419856+H-chan03@users.noreply.github.com>
2024-08-16 16:00:57 +0900

    #1.mbti pages~

---
+++ js/results.js
@@ -0,0 +1,37 @@
+import { results, mbtis } from "./data.js";
+const mbti = new URLSearchParams(location.search).get('mbti')
+console.log(location.search)
+console.log(mbti);
+
+// 1. 가져온 mbti 유형의 번호를 출력
+// ?mbti=istp ===> 2인 것을 출력
+console.log(mbtis[mbti])
+
+// 2. 위의 results 중 1번 결과에 해당하는 자료를 출력
+console.log(results[mbtis[mbti]])
+let result = results[mbtis[mbti]];
+
+// 3. 그 중 결과의 제목을 출력
+console.log(results[mbtis[mbti]].title)
+
+// 4. querySelector로 변수 선언
+// --- innerHTML로 넣기
+const title = document.querySelector(".page-title");
+const result_character = document.querySelector('.character');
+const boxes = document.querySelectorAll('.box');
+const jobs = document.querySelectorAll('.job');
+
+const lectureURL = document.querySelector('.lecture');
+const lectureImage = document.querySelector('.lecture img');
+
+title.innerHTML = result.title;
+result_character = result.character;
+boxes.forEach(function(boxEl, index) {
+    boxEl.innerHTML = result.results[index];
+})
+jobs.forEach((job, index)=>{
+    job.innerHTML = result.jobs[index];
+})
+
+lectureImage.src = result.lectureImg;
+lectureURL.href = result.lectureUrl;
